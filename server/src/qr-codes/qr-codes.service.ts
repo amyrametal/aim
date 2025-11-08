b6492d20e823c1fe1b673fb2a2636bbd7557e18a
Amyra <amyramttl@gmail.com>
2025-11-09 04:48:21 +0530

    Use In() for bulk find in bulkCreate method

--- server/src/qr-codes/qr-codes.service.ts
+++ server/src/qr-codes/qr-codes.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { QrCode } from './qr-code.entity';
+import { In } from 'typeorm';
+
 
 @Injectable()
 export class QrCodesService {
   constructor(@InjectRepository(QrCode) private repo: Repository<QrCode>) {}
 
   async bulkCreate(codes: string[]) {
-    const existing = await this.repo.findBy({ code: codes });
+    const existing = await this.repo.findBy({ code: In(codes) });
     const set = new Set(existing.map(e => e.code));
     const toIns = codes.filter(c => !set.has(c)).map(code => ({ code }));
     await this.repo.insert(toIns);
