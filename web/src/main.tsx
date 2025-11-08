3a534ea8ccbeac95619e4b5c30c83da3ea2214bd
Amyra <amyramttl@gmail.com>
2025-11-09 04:09:25 +0530

    Initialize main.tsx with React setup and service worker

---
+++ web/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+// register service-worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js');
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
