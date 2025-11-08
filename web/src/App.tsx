86a4be497b30d0bbeb8f7b74d4187428bde0847c
Amyra <amyramttl@gmail.com>
2025-11-09 04:20:18 +0530

    Add VaccineManager route to App component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ClaimQr from './pages/ClaimQr';
 import AnimalForm from './pages/AnimalForm';
-
+import VaccineManager from './pages/VaccineManager';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ClaimQr />} />
         <Route path="/animal/new" element={<AnimalForm />} />
+        <Route path="/animal/:animalId/vaccines" element={<VaccineManager />} />
       </Routes>
     </BrowserRouter>
   );
