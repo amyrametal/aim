babf38bee5aca7c7c7b90144a4ec22cdc20043f2
Amyra <amyramttl@gmail.com>
2025-11-09 04:30:10 +0530

    Fix route parameter for VaccineManager component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,15 +4,16 @@ import AnimalForm from './pages/AnimalForm';
 import AnimalList from './pages/AnimalList';
 import AnimalDetail from './pages/AnimalDetail';
 import VaccineManager from './pages/VaccineManager';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ClaimQr />} />
         <Route path="/animal/new" element={<AnimalForm />} />
-        <Route path="/animal/:animalId/vaccines" element={<VaccineManager />} />
         <Route path="/animals" element={<AnimalList />} />
         <Route path="/animal/:nanoid" element={<AnimalDetail />} />
+        <Route path="/animal/:animalId/vaccines" element={<VaccineManager />} />
       </Routes>
     </BrowserRouter>
   );
