b70003a4fbc7146125f51ca2ac35ff2741d4b475
Amyra <amyramttl@gmail.com>
2025-11-09 04:23:07 +0530

    Add routes for AnimalList and AnimalDetail pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ClaimQr from './pages/ClaimQr';
 import AnimalForm from './pages/AnimalForm';
+import AnimalList from './pages/AnimalList';
+import AnimalDetail from './pages/AnimalDetail';
 import VaccineManager from './pages/VaccineManager';
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Route path="/" element={<ClaimQr />} />
         <Route path="/animal/new" element={<AnimalForm />} />
         <Route path="/animal/:animalId/vaccines" element={<VaccineManager />} />
+        <Route path="/animals" element={<AnimalList />} />
+        <Route path="/animal/:nanoid" element={<AnimalDetail />} />
       </Routes>
     </BrowserRouter>
   );
